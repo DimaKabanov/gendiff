5477d8bfb298b3d2f438a41297961f4e0483cb1f
Дмитрий Кабанов <dmitrykabanov@Mini-Dmitrij.jetstyle.int>
2018-05-14 16:06:26 +0500

    change version for commander

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@
 import commander from 'commander';
 
 commander
-  .version('0.0.1')
+  .version('0.0.2')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
   .description('Compares two configuration files and shows a difference.')
