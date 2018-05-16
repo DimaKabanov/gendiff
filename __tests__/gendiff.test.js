5bffe916d985b27235a352ed6b2bddf9960eff2d
Dmitry Kabanov <dkabanov6@gmail.com>
2018-05-16 21:18:31 +0500

    added tests for yaml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,9 +6,18 @@ const pathToCorrectOutput = '__tests__/__fixtures__/correctOutput';
 const pathToBeforeJson = '__tests__/__fixtures__/before.json';
 const pathToAfterJson = '__tests__/__fixtures__/after.json';
 
+const pathToBeforeYaml = '__tests__/__fixtures__/before.yml';
+const pathToAfterYaml = '__tests__/__fixtures__/after.yml';
+
 describe('GenDiff', () => {
   test('check json', () => {
     const actual = genDiff(pathToBeforeJson, pathToAfterJson);
+    const expected = fs.readFileSync(pathToCorrectOutput, 'utf8');
+    expect(actual).toBe(expected);
+  });
+
+  test('check yaml', () => {
+    const actual = genDiff(pathToBeforeYaml, pathToAfterYaml);
     const expected = fs.readFileSync(pathToCorrectOutput, 'utf8');
     expect(actual).toBe(expected);
   });
