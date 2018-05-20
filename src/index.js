e483f972949452cdff9ee3d790be04dc8000836f
Dmitry Kabanov <dkabanov6@gmail.com>
2018-05-20 09:42:14 +0500

    made refactoring create ast

--- src/index.js
+++ src/index.js
@@ -4,66 +4,73 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import getParser from './parser';
+import getRender from './renderings';
 
 const propertyActions = [
   {
     type: 'added',
-    check: ({ objBefore, key }) => !_.has(objBefore, key),
-    extendNode: ({ objAfter, key, depth }) => ({ value: objAfter[key], depth }),
+    check: ({ before }) => !before,
+    buildNode: ({ type, key, after, depth }) => ({ type, key, after, depth }),
   },
   {
     type: 'deleted',
-    check: ({ objAfter, key }) => !_.has(objAfter, key),
-    extendNode: ({ objBefore, key, depth }) => ({ value: objBefore[key], depth }),
+    check: ({ after }) => !after,
+    buildNode: ({ type, key, before, depth }) => ({ type, key, before, depth }),
   },
   {
     type: 'nested',
-    check: ({ objBefore, objAfter, key }) =>
-      _.isPlainObject(objBefore[key]) && _.isPlainObject(objAfter[key]),
-    extendNode: ({ objBefore, objAfter, key, depth, createAst }) => // eslint-disable-line
-      ({ children: [...createAst(objBefore[key], objAfter[key], depth + 1)], depth }),
+    check: ({ before, after }) => _.isPlainObject(before) && _.isPlainObject(after),
+    buildNode: ({ type, key, before, after, depth, buildAst }) =>
+      ({ type, key, depth, children: buildAst(before, after, depth + 1) }),
   },
   {
     type: 'unchanged',
-    check: ({ objBefore, objAfter, key }) => objBefore[key] === objAfter[key],
-    extendNode: ({ objBefore, key, depth }) => ({ value: objBefore[key], depth }),
+    check: ({ before, after }) => before === after,
+    buildNode: ({ type, key, before, depth }) => ({ type, key, before, depth }),
   },
   {
-    type: 'changed',
-    check: ({ objBefore, objAfter, key }) => objBefore[key] !== objAfter[key],
-    extendNode: ({ objBefore, objAfter, key, depth }) => // eslint-disable-line
-      ({ value: { newValue: objAfter[key], oldValue: objBefore[key] }, depth }),
+    check: ({ before, after }) => before !== after,
+    buildNode: ({ key, after, before, depth }) => {
+      const oldNode = { type: 'deleted', key, before, depth };
+      const newNode = { type: 'added', key, after, depth };
+      return [oldNode, newNode];
+    },
   },
 ];
 
-const getPropertyAction = (objBefore, objAfter, key) =>
-  _.find(propertyActions, ({ check }) => check({ objBefore, objAfter, key }));
+const getPropertyAction = (before, after) =>
+  _.find(propertyActions, ({ check }) => check({ before, after }));
 
-const createAst = (objBefore, objAfter, depth = 1) => {
+const buildAst = (objBefore, objAfter, depth = 1) => {
   const uniqKeys = _.union(Object.keys(objBefore), Object.keys(objAfter));
 
   return uniqKeys.map((key) => {
-    const { extendNode, type } = getPropertyAction(objBefore, objAfter, key);
+    const before = objBefore[key];
+    const after = objAfter[key];
 
-    return { ...extendNode({ objBefore, objAfter, key, depth, createAst }), name: key, type }; // eslint-disable-line
+    const { type, buildNode } = getPropertyAction(before, after);
+
+    return buildNode({ type, key, before, after, depth, buildAst });
   });
 };
 
-const genDiff = (pathToFileBefore: string, pathToFileAfter: string, render: any): string => {
+const genDiff = (pathToFileBefore: string, pathToFileAfter: string, format: string): string => {
   const dataFileBefore = fs.readFileSync(pathToFileBefore, 'utf8');
   const dataFileAfter = fs.readFileSync(pathToFileAfter, 'utf8');
 
   const extFileBefore = path.extname(pathToFileBefore);
   const extFileAfter = path.extname(pathToFileAfter);
 
-  const parserData = getParser(extFileBefore, extFileAfter);
+  const getParseredData = getParser(extFileBefore, extFileAfter);
 
-  const objBefore = parserData(dataFileBefore);
-  const objAfter = parserData(dataFileAfter);
+  const objBefore = getParseredData(dataFileBefore);
+  const objAfter = getParseredData(dataFileAfter);
 
-  const ast = createAst(objBefore, objAfter);
+  const ast = buildAst(objBefore, objAfter);
 
-  return render(ast);
+  const getRenderedData = getRender(format);
+
+  return getRenderedData(ast);
 };
 
 export default genDiff;
